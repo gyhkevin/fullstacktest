6d0447f4c83e310ba010130eded89076f1330c20
gyhkevin <haiven1989@126.com>
2016-08-07 18:49:09 +0800

    sass and compass to create a page of responsive

---
+++ index.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Contact Us</title>
+	<link rel="stylesheet" type="text/css" href="libs/uikit/css/uikit.almost-flat.min.css">
+	<link rel="stylesheet" type="text/css" href="libs/uikit/css/uikit.gradient.min.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<!-- <script src="//cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script> -->
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<script type="text/javascript" src="libs/uikit/js/uikit.min.js"></script>
+	<script type="text/javascript" src="libs/uikit/js/components/parallax.min.js"></script>
+</head>
+<body>
+	<div class="mynavbar">
+		<div class="navbar_left">
+			<a href="#offcanvas" class="uk-navbar-toggle bg_menu" data-uk-offcanvas></a>
+			<a class="uk-navbar-brand" href="#"><img src="images/logo1.png"></a>
+		</div>
+		<div class="navbar_right">
+			<p><span class="uk-icon-phone"></span> +86 186 7557 6605</p>
+		</div>
+	</div>
+	<header class="header" data-uk-parallax="bg:100">
+		<div class="header-inner" data-uk-parallax="{opacity:0.5, viewport:0.9, y:100}">
+			<h2>FLY ASH</h2>
+			<h2 class="text_bold">TECHNOLOGY SOLUTIONS</h2>
+			<h2 class="text_assert"> Systema Leonardo <sup>TM</sup></h2>
+			<p>Helping power stations to go green while converting their environmental liability 
+into a cash generating asset</p>
+		</div>
+	</header>
+
+	<div class="uk-panel-space"></div>
+	<div class="uk-container uk-container-center">
+		<ul class="uk-grid uk-grid-small uk-grid-width-large-1-2 uk-grid-width-medium-1-1 uk-grid-width-small-1-1" data-uk-grid-margin>
+			<li></li>
+			<li class="text_logo_float">
+				<h2 class="text_logo"> Systema Leonardo <sup>TM</sup></h2>
+			</li>
+			<li>
+				<div class="chapter_v">
+					<figure class="uk-overlay uk-overlay-hover">
+					    <img src="images/video1.jpg" width="" height="" alt="">
+					    <div class="uk-overlay-panel uk-overlay-fade uk-overlay-background">
+					    	<figcaption class="uk-overlay-panel">
+					    		<span class="uk-icon-play-circle video_play"></span>
+					    	</figcaption>
+					    </div>
+					    <a class="uk-position-cover" href=""></a>
+					</figure>
+				</div>
+			</li>
+			<li>
+				<div class="chapter_v">
+					<p>So, you have some good precipitators at your power plant ensuring 
+					that there is little to no smoke entering the atmosphere 
+					but now you have mountains of ash piling up. 
+					You'd rather not dump it but the cement industry can not utilise all of it. 
+					What choice do you have?</p>
+					<p>Vecor's Systema LeonardoTM solution forms a cornerstone of Vecor's mission 
+					to assist power stations to comprehensively utilise their fly ash resources. 
+					Watch our video to find out how your power station can:</p>
+					<ul class="list_good">
+						<li><span class="uk-icon-check-circle"></span>Improve environmental CSR</li>
+						<li><span class="uk-icon-check-circle"></span>Eliminate dumping costs</li>
+						<li><span class="uk-icon-check-circle"></span>Enjoy a new revenue stream</li>
+					</ul>
+				</div>
+			</li>
+		</ul>
+	</div>
+	<div class="uk-panel-space"></div>
+	<div class="longterm">
+		<div class="uk-container uk-container-center">
+			<div class="middle-inner">
+				<h2>LONG TERM PARTNERSHIP </h2>
+				<h2 class="text_bold">FOR TRIPLE BOTTOM LINE SUCCESS</h2>
+			</div>
+			
+			<ul class="uk-grid uk-grid-large uk-grid-width-large-1-4 uk-grid-width-medium-1-2 uk-grid-width-small-1-1 partner_grid" data-uk-grid-margin >
+				<li class="uk-text-center">
+					<div class="partner">
+						<img src="images/icon1.png">
+						<h3>COMPREHENSIVE SUPPORT</h3>
+						<p>24/7 stewardship, regular training and permanent on-site technical director</p>
+					</div>
+				</li>
+				<li class="uk-text-center">
+					<div class="partner">
+						<img src="images/icon2.png">
+						<h3>VICS TM<span style=""></span></h3>
+						<p>Online, real-time data analysis and process management</p>
+					</div>
+				</li>
+				<li class="uk-text-center">
+					<div class="partner">
+						<img src="images/icon3.png">
+						<h3>TILES OFFTAKE PROGRAM</h3>
+						<p>Vecor takes all marketing and distribution risk</p>
+					</div>
+				</li>
+				<li class="uk-text-center">
+					<div class="partner">
+						<img src="images/icon4.png">
+						<h3>FINANCING SOLUTIONS</h3>
+						<p>Flexible partnership model to suit individual situations</p>
+					</div>
+				</li>
+			</ul>
+			<p class="uk-text-middle uk-text-right">Vecor's own Systema Leonardo ceramic tile factory under construction in China. Designed to accept up to 70% recycled aluminium silicates in the tile body.</p>
+		</div>
+	</div>
+	<div class="uk-panel-space"></div>
+	<div class="uk-container uk-container-center">
+		<ul class="uk-grid uk-grid-large uk-grid-width-large-1-2 uk-grid-width-medium-1-2 uk-grid-width-small-1-1" data-uk-grid-margin>
+			<li>
+				<div class="contact">
+					Contact us
+				</div>
+			</li>
+			<li>
+				<div class="">
+					<form action="contactus" method="post">
+						<input type="email" name="email" value="" placeholder="EMAIL" class="uk-form-help-block uk-form-width-large">
+						<textarea name="message" placeholder="MESSAGE" class="uk-form-help-block uk-form-width-large"></textarea>
+						<input type="submit" value="submit">
+					</form>	
+				</div>
+			</li>
+		</ul>
+	</div>
+	<div class="uk-panel-space"></div>
+
+	<div id="offcanvas" class="uk-offcanvas">
+		<div class="uk-offcanvas-bar">
+			<ul class="uk-nav uk-nav-offcanvas">
+				<li><a href="#" title="">menu_1</a></li>
+				<li><a href="#" title="">menu_2</a></li>
+				<li><a href="#" title="">menu_3</a></li>
+			</ul>
+		</div>
+	</div>
+</body>
+</html>
